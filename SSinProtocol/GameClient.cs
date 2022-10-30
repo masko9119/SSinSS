cb41affd48ace6ddb0fff67e9eb6d2d97aa3626d
masko9119 <masko9119@gmail.com>
2022-10-30 19:38:37 +0100

    Update GameClient.cs

--- SSinProtocol/GameClient.cs
+++ SSinProtocol/GameClient.cs
@@ -144,7 +144,7 @@ namespace PROProtocol
         public event Action ActivePokemonChanged;
         public event Action OpponentChanged;
         
-        private const string Version = "Halloween20_v2";
+        private const string Version = "Halloween22_v2";
 
         private GameConnection _connection;
         private DateTime _lastMovement;
